10e4be9d2f26445ce10cdfc9e392c1700a815db0
Srinath Rayabarapu <naath.r@gmail.com>
2018-12-06 18:30:13 +0530

    Initial commit

---
+++ RESTFulJersey/src/com/javatpoint/rest/ChildHello.java
@@ -0,0 +1,18 @@
+package com.javatpoint.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/child")
+public class ChildHello extends SuperHello {
+
+    @GET
+    @Path("/jersey1")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello child Jersey Plain";
+    }
+
+}
